e059272082354665c4581697a54e96356362f4d4
Vincent Lafeychine <vincent.lafeychine@proton.me>
2024-03-26 10:36:18 +0100

    chore(na): Cpre/Cpost takes rt_cur instead of rt_full

--- theories/rust_typing/existentials_na.v
+++ theories/rust_typing/existentials_na.v
@@ -393,7 +393,7 @@ Section generated_code.
 
 
   Section na_subtype.
-    Context `{!refinedrustGS Σ}.
+    Context `{!typeGS Σ}.
     Context {rt X : Type} (P : na_ex_inv_def rt X).
 
     Lemma na_owned_subtype_ex_plain_t π E L (ty : type rt) (r : rt) (r' : X) T :
@@ -549,9 +549,11 @@ Section generated_code.
         li_tactic (lctx_lft_alive_count_goal E L1 κ)  (λ '(_, L2),
           ∀ r, introduce_with_hooks E L2 (P.(na_inv_P) π r x)
             (λ L3, typed_place π E L3 l
-                    (* TODO: (MagicLtype (◁ ty) (◁ (∃na; P, ty)) INV_MASK (λ mask rfn, P.(na_inv_P) π rfn x ∗ na_own π (⊤ minus ...)) (λ rfn, na_own π ⊤) (* TODO: Looks more OpenLTyped *) *)
-                    (ShadowedLtype (◁ ty) #r (◁ (∃na; P, ty)))
-                    (#x) bmin (Owned true) K T)))
+                    (@MagicLtype Σ typeGS0 rt X (◁ ty) (◁ (∃na; P, ty)) (λ mask rfn, P.(na_inv_P) π rfn x ∗ na_own π mask) (λ mask rfn, na_own π mask))
+                    (#r) bmin (Owned true) K
+            (λ L2 κs li b2 bmin' rti ltyi ri strong weak,
+              T L2 κs li b2 bmin' rti ltyi ri strong None)
+        )))
       ⊢ typed_place π E L l (◁ (∃na; P, ty))%I (#x) bmin (Shared κ) K T.
     Proof.
       rewrite /prove_with_subtype.
