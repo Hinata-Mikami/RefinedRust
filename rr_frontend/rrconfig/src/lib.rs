d75cd119ac967d6a6ce3b2fc586385d831f73a66
Vincent Lafeychine <vincent.lafeychine@proton.me>
2025-06-29 00:24:23 +0200

    refactor(rrconfig): Use an explicit struct

--- rr_frontend/rrconfig/src/lib.rs
+++ rr_frontend/rrconfig/src/lib.rs
@@ -1,320 +1,330 @@
-// © 2019, ETH Zurich
+// © 2025, The RefinedRust Developers and Contributors
 //
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the BSD-3-clause License.
+// If a copy of the BSD-3-clause license was not distributed with this
+// file, You can obtain one at https://opensource.org/license/bsd-3-clause/.
 
+use std::env;
 use std::path::PathBuf;
-use std::sync::{LazyLock, RwLock};
-use std::{env, mem};
+use std::sync::{LazyLock, RwLock, RwLockWriteGuard};
 
-use config::{Config, Environment, File, FileFormat};
 use path_clean::PathClean as _;
 use serde::Deserialize;
 
-// RwLock due to rustc parallelism
-static SETTINGS: LazyLock<RwLock<Config>> = LazyLock::new(|| {
-    RwLock::new({
-        let mk_config = || {
-            let mut builder = Config::builder();
-
-            // 1. Default values
-            builder = builder
-                .set_default("be_rustc", false)?
-                .set_default("log_dir", "./log/")?
-                .set_default("check_overflows", true)?
-                .set_default("dump_debug_info", false)?
-                .set_default("dump_borrowck_info", false)?
-                .set_default("quiet", false)?
-                .set_default("skip_unsupported_features", true)?
-                .set_default("spec_hotword", "rr")?
-                .set_default("attribute_parser", "verbose")?
-                .set_default("run_check", false)?
-                .set_default("verify_deps", false)?
-                .set_default("no_verify", false)?
-                .set_default("cargo_path", "cargo")?
-                .set_default("cargo_command", "check")?
-                .set_default("admit_proofs", false)?
-                .set_default("generate_dune_project", true)?
-                .set_default("lib_load_paths", Vec::<String>::new())?
-                .set_default("work_dir", ".")?;
-
-            // 2. Override with the optional TOML file "RefinedRust.toml" (if there is any)
-            builder = builder.add_source(File::new("RefinedRust.toml", FileFormat::Toml).required(false));
-
-            // 3. Override with an optional TOML file specified by the `RR_CONFIG` env variable
-            if let Ok(file) = env::var("RR_CONFIG") {
-                // Since this file is explicitly specified by the user, it would be
-                // nice to tell them if we cannot open it.
-                builder = builder.add_source(File::with_name(&file));
-
-                // set override for workdir to the config file path
-                let path_to_file = PathBuf::from(file);
-                let filepath = path_to_file.parent().unwrap().to_str().unwrap();
-
-                builder = builder.set_default("work_dir", filepath)?;
-            }
-
-            // 4. Override with env variables (`RR_QUIET`, ...)
-            let builder = builder.add_source(Environment::with_prefix("RR").ignore_empty(true));
-
-            builder.build()
-        };
-
-        mk_config().unwrap()
-    })
-});
-
-/// Generate a dump of the settings
-#[must_use]
-pub fn dump() -> String {
-    format!("{:#?}", SETTINGS.read().unwrap())
+#[derive(Debug, Deserialize)]
+#[serde(default)]
+#[expect(clippy::struct_excessive_bools)]
+struct Config {
+    be_rustc: bool,
+    log_dir: String,
+    check_overflows: bool,
+    dump_debug_info: bool,
+    dump_borrowck_info: bool,
+    quiet: bool,
+    skip_unsupported_features: bool,
+    spec_hotword: String,
+    attribute_parser: String,
+    run_check: bool,
+    verify_deps: bool,
+    no_verify: bool,
+    cargo_path: String,
+    cargo_command: String,
+    admit_proofs: bool,
+    generate_dune_project: bool,
+    lib_load_paths: Vec<String>,
+    work_dir: String,
+    output_dir: Option<String>,
+    post_generation_hook: Option<String>,
+    shims: Option<String>,
+    extra_specs: Option<String>,
 }
 
-/// Makes the path absolute with respect to the `work_dir`.
-fn make_path_absolute(path: &str) -> PathBuf {
-    // read the base path we set
-    let base_path = work_dir();
-
-    let path_buf = PathBuf::from(path);
-    if path_buf.is_absolute() {
-        path_buf
-    } else {
-        let base_path_buf = PathBuf::from(base_path);
-        if base_path_buf.is_absolute() {
-            base_path_buf.join(path_buf).clean()
-        } else {
-            env::current_dir().unwrap().join(base_path_buf).join(path_buf).clean()
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            be_rustc: false,
+            log_dir: "./log/".to_owned(),
+            check_overflows: true,
+            dump_debug_info: false,
+            dump_borrowck_info: false,
+            quiet: false,
+            skip_unsupported_features: true,
+            spec_hotword: "rr".to_owned(),
+            attribute_parser: "verbose".to_owned(),
+            run_check: false,
+            verify_deps: false,
+            no_verify: false,
+            cargo_path: "cargo".to_owned(),
+            cargo_command: "check".to_owned(),
+            admit_proofs: false,
+            generate_dune_project: true,
+            lib_load_paths: vec![],
+            work_dir: ".".to_owned(),
+            output_dir: None,
+            post_generation_hook: None,
+            shims: None,
+            extra_specs: None,
         }
     }
 }
 
-fn read_optional_setting<T>(name: &'static str) -> Option<T>
-where
-    T: Deserialize<'static>,
-{
-    SETTINGS.read().unwrap().get(name).ok()
+static SETTINGS: LazyLock<RwLock<Config>> = LazyLock::new(|| {
+    // RwLock due to `rustc` parallelism
+    RwLock::new({
+        let mut builder = config::Config::builder();
+
+        // 1. Read the optional TOML file "RefinedRust.toml", if there is any
+        builder = builder.add_source(config::File::with_name("RefinedRust.toml").required(false));
+
+        // 2. Override with an optional TOML file specified by the `RR_CONFIG` env variable
+        if let Ok(file) = env::var("RR_CONFIG") {
+            // set override for workdir to the config file path
+            let path_to_file = PathBuf::from(file.clone());
+            let filepath = path_to_file.parent().unwrap().to_str().unwrap();
+
+            builder = builder.set_default("work_dir", filepath).unwrap();
+
+            // Since this file is explicitly specified by the user, it would be
+            // nice to tell them if we cannot open it.
+            builder = builder.add_source(config::File::with_name(&file));
+        }
+
+        // 3. Override with env variables (`RR_QUIET`, ...)
+        let builder = builder.add_source(config::Environment::with_prefix("RR").ignore_empty(true));
+
+        let config = builder.build().unwrap();
+
+        config.try_deserialize().unwrap()
+    })
+});
+
+/// Makes the path absolute with respect to the `work_dir`.
+fn make_path_absolute(path: &str) -> PathBuf {
+    let path_buf = PathBuf::from(path);
+    if path_buf.is_absolute() {
+        return path_buf;
+    }
+
+    // read the base path we set
+    let base_path_buf = PathBuf::from(work_dir());
+    if base_path_buf.is_absolute() {
+        base_path_buf.join(path_buf).clean()
+    } else {
+        env::current_dir().unwrap().join(base_path_buf).join(path_buf).clean()
+    }
 }
 
-fn read_setting<T>(name: &'static str) -> T
-where
-    T: Deserialize<'static>,
-{
-    read_optional_setting(name).unwrap_or_else(|| panic!("Failed to read setting {:?}", name))
+fn access_config<'a, T, C: FnOnce(RwLockWriteGuard<'a, Config>) -> T>(callback: C) -> T {
+    callback(SETTINGS.write().unwrap())
 }
 
-fn write_setting<T: Into<config::Value>>(key: &'static str, value: T) {
-    let builder = Config::builder().set_override(key, value).unwrap();
-    let mut w = SETTINGS.write().unwrap();
-    let old_settings = mem::take(&mut *w);
-    let new_settings = builder.add_source(old_settings).build().unwrap();
-    *w = new_settings;
-}
-
+/// Should refinedrust-rustc behave like rustc?
 #[must_use]
-pub fn work_dir() -> String {
-    read_setting("work_dir")
-}
-
-#[must_use]
-pub fn absolute_work_dir() -> PathBuf {
-    let s = work_dir();
-    make_path_absolute(&s)
-}
-
-/// Should we dump debug files?
-#[must_use]
-pub fn dump_debug_info() -> bool {
-    read_setting("dump_debug_info")
-}
-
-/// Should we dump borrowck info?
-#[must_use]
-pub fn dump_borrowck_info() -> bool {
-    read_setting("dump_borrowck_info")
+pub fn be_rustc() -> bool {
+    access_config(|c| c.be_rustc)
 }
 
 /// In which folder should we store log/dumps?
 #[must_use]
 pub fn log_dir() -> PathBuf {
-    make_path_absolute(&read_setting::<String>("log_dir"))
+    make_path_absolute(&access_config(|c| c.log_dir.clone()))
+}
+
+/// Should we check for overflows?
+#[must_use]
+pub fn check_overflows() -> bool {
+    access_config(|c| c.check_overflows)
+}
+
+/// Should we dump debug files?
+#[must_use]
+pub fn dump_debug_info() -> bool {
+    access_config(|c| c.dump_debug_info)
+}
+
+/// Should we dump borrowck info?
+#[must_use]
+pub fn dump_borrowck_info() -> bool {
+    access_config(|c| c.dump_borrowck_info)
+}
+
+/// Should we hide user messages?
+#[must_use]
+pub fn quiet() -> bool {
+    access_config(|c| c.quiet)
+}
+
+/// Skip features that are unsupported or partially supported
+#[must_use]
+pub fn skip_unsupported_features() -> bool {
+    access_config(|c| c.skip_unsupported_features)
+}
+
+/// The hotword with which specification attributes should begin.
+#[must_use]
+pub fn spec_hotword() -> String {
+    access_config(|c| c.spec_hotword.clone())
+}
+
+/// Which attribute parser to use? Currently, only the "verbose" parser is supported.
+#[must_use]
+pub fn attribute_parser() -> String {
+    access_config(|c| c.attribute_parser.clone())
+}
+
+/// Run the proof checker after generating the Coq code?
+#[must_use]
+pub fn check_proofs() -> bool {
+    access_config(|c| c.run_check)
+}
+
+/// Should also dependencies be verified?
+#[must_use]
+pub fn verify_deps() -> bool {
+    access_config(|c| c.verify_deps)
+}
+
+/// Should verification be skipped?
+#[must_use]
+pub fn no_verify() -> bool {
+    access_config(|c| c.no_verify)
+}
+
+pub fn set_no_verify(value: bool) {
+    access_config(|mut c| c.no_verify = value);
+}
+
+/// Which cargo to use?
+#[must_use]
+pub fn cargo_path() -> String {
+    access_config(|c| c.cargo_path.clone())
+}
+
+/// Which cargo command should cargo-refinedrust hook into?
+#[must_use]
+pub fn cargo_command() -> String {
+    access_config(|c| c.cargo_command.clone())
+}
+
+/// Whether to admit proofs of functions instead of running Qed.
+#[must_use]
+pub fn admit_proofs() -> bool {
+    access_config(|c| c.admit_proofs)
+}
+
+/// Whether to generate a dune-project file for this project
+#[must_use]
+pub fn generate_dune_project() -> bool {
+    access_config(|c| c.generate_dune_project)
 }
 
 /// Get the paths in which to search for `RefinedRust` library declarations.
 #[must_use]
 pub fn lib_load_paths() -> Vec<PathBuf> {
     let mut paths = Vec::new();
-    let s = read_setting::<Vec<String>>("lib_load_paths");
+    let s = access_config(|c| c.lib_load_paths.clone());
     for p in s {
         paths.push(make_path_absolute(&p));
     }
     paths
 }
 
-/// The hotword with which specification attributes should begin.
 #[must_use]
-pub fn spec_hotword() -> String {
-    read_setting("spec_hotword")
+pub fn work_dir() -> String {
+    access_config(|c| c.work_dir.clone())
 }
 
-/// Should we hide user messages?
 #[must_use]
-pub fn quiet() -> bool {
-    read_setting("quiet")
-}
-
-/// Skip features that are unsupported or partially supported
-#[must_use]
-pub fn skip_unsupported_features() -> bool {
-    read_setting("skip_unsupported_features")
-}
-
-/// Whether to generate a dune-project file for this project
-#[must_use]
-pub fn generate_dune_project() -> bool {
-    read_setting("generate_dune_project")
-}
-
-/// Which attribute parser to use? Currently, only the "verbose" parser is supported.
-#[must_use]
-pub fn attribute_parser() -> String {
-    read_setting("attribute_parser")
+pub fn absolute_work_dir() -> PathBuf {
+    make_path_absolute(&work_dir())
 }
 
 /// Which directory to write the generated Coq files to?
 #[must_use]
 pub fn output_dir() -> Option<PathBuf> {
-    read_optional_setting("output_dir").map(|s: String| make_path_absolute(&s))
-}
-
-/// Whether to admit proofs of functions instead of running Qed.
-#[must_use]
-pub fn admit_proofs() -> bool {
-    read_setting("admit_proofs")
+    access_config(|c| c.output_dir.clone()).map(|s: String| make_path_absolute(&s))
 }
 
 /// Post-generation hook
 #[must_use]
 pub fn post_generation_hook() -> Option<String> {
-    read_optional_setting("post_generation_hook")
+    access_config(|c| c.post_generation_hook.clone())
 }
 
 /// Which file to read shims from?
 #[must_use]
 pub fn shim_file() -> Option<PathBuf> {
-    read_optional_setting("shims").map(|s: String| make_path_absolute(&s))
+    access_config(|c| c.shims.clone()).map(|s: String| make_path_absolute(&s))
 }
 
 /// Which file should we read extra specs from?
 #[must_use]
 pub fn extra_specs_file() -> Option<PathBuf> {
-    read_optional_setting("extra_specs").map(|s: String| make_path_absolute(&s))
-}
-
-/// Run the proof checker after generating the Coq code?
-#[must_use]
-pub fn check_proofs() -> bool {
-    read_setting("run_check")
-}
-
-/// Which cargo to use?
-#[must_use]
-pub fn cargo_path() -> String {
-    read_setting("cargo_path")
-}
-
-/// Which cargo command should cargo-refinedrust hook into?
-#[must_use]
-pub fn cargo_command() -> String {
-    read_setting("cargo_command")
-}
-
-/// Should refinedrust-rustc behave like rustc?
-#[must_use]
-pub fn be_rustc() -> bool {
-    read_setting("be_rustc")
-}
-
-/// Should also dependencies be verified?
-#[must_use]
-pub fn verify_deps() -> bool {
-    read_setting("verify_deps")
-}
-
-/// Should verification be skipped?
-#[must_use]
-pub fn no_verify() -> bool {
-    read_setting("no_verify")
-}
-
-pub fn set_no_verify(value: bool) {
-    write_setting("no_verify", value);
-}
-
-/// Should we check for overflows?
-#[must_use]
-pub fn check_overflows() -> bool {
-    read_setting("check_overflows")
+    access_config(|c| c.extra_specs.clone()).map(|s: String| make_path_absolute(&s))
 }
 
 #[cfg(test)]
+#[expect(clippy::bool_assert_comparison)]
 mod tests {
+    use std::sync::Once;
+
     use super::*;
 
-    // NOTE: These tests SHOULD NOT use the same key as the same SETTINGS variable is used
+    // These tests MUST use different keys for each test.
+    // The same SETTINGS variable is used throughout.
+
+    // There is a race between `std::env::set_var()` and `SETTINGS`.
+    static SETUP_ENV: Once = Once::new();
+
+    fn setup<F: FnOnce(RwLockWriteGuard<'_, Config>)>(test: F) {
+        SETUP_ENV.call_once(|| {
+            // Safety: This block is single-threaded
+            #[expect(clippy::multiple_unsafe_ops_per_block)]
+            unsafe {
+                env::set_var("RR_log_dir", "12345");
+                env::set_var("RR_ChEcK_oVeRfLoWs", "false");
+                env::set_var("RR_dump_debug_info", "1");
+            }
+        });
+
+        access_config(test);
+    }
 
     #[test]
     fn read_default() {
-        assert_eq!(read_setting::<String>("work_dir"), ".");
+        setup(|c| assert_eq!(c.be_rustc, false));
     }
 
     #[test]
-    fn read_env() {
-        // Safety: Test is single-threaded
-        unsafe {
-            env::set_var("RR_simple_read", "true");
-        }
-
-        assert!(read_setting::<bool>("simple_read"));
+    fn read_env_str() {
+        setup(|c| assert_eq!(c.log_dir.clone(), "12345"));
     }
 
     #[test]
-    fn read_env_case() {
-        // Safety: Test is single-threaded
-        unsafe {
-            env::set_var("RR_CaSe_ChEcK", "true");
-        }
-
-        assert!(read_setting::<bool>("case_check"));
+    fn read_env_bool() {
+        setup(|c| assert_eq!(c.check_overflows, false));
     }
 
     #[test]
-    fn read_env_value_bool() {
-        // Safety: Test is single-threaded
-        unsafe {
-            env::set_var("RR_CaSe_ChEcK", "1");
-        }
-
-        assert!(read_setting::<bool>("case_check"));
+    fn read_env_int_as_bool() {
+        setup(|c| assert_eq!(c.dump_debug_info, true));
     }
 
     #[test]
     fn write() {
-        write_setting("key", true);
+        setup(|mut c| c.quiet = true);
+        setup(|c| assert_eq!(c.quiet, true));
 
-        assert!(read_setting::<bool>("key"));
-
-        write_setting("key", false);
-
-        assert!(!(read_setting::<bool>("key")));
+        setup(|mut c| c.quiet = false);
+        setup(|c| assert_eq!(c.quiet, false));
     }
 
     #[test]
-    fn write_merge() {
-        write_setting("key1", "value1");
-        write_setting("key2", "value2");
+    fn write_multiple() {
+        setup(|mut c| c.skip_unsupported_features = false);
+        setup(|c| assert_eq!(c.skip_unsupported_features, false));
 
-        assert_eq!(read_setting::<String>("key1"), "value1");
-        assert_eq!(read_setting::<String>("key2"), "value2");
+        setup(|mut c| c.spec_hotword = "12345".to_owned());
+        setup(|c| assert_eq!(c.spec_hotword, "12345"));
     }
 }
