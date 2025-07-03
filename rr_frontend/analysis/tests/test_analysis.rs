aef2f31bf77cb23dc125c9558486d60d85854e75
Vincent Lafeychine <vincent.lafeychine@proton.me>
2025-07-03 11:13:56 +0200

    chore(analysis): Remove test-binary crate

--- rr_frontend/analysis/tests/test_analysis.rs
+++ rr_frontend/analysis/tests/test_analysis.rs
@@ -1,68 +1,38 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(test_runner)]
 
-use std::env;
 use std::path::PathBuf;
+use std::{env, process};
 
-/// Source: <https://github.com/rust-lang/miri/blob/master/tests/compiletest.rs>
 use compiletest_rs as compiletest;
-use test_binary::build_test_binary_once;
 
-fn find_sysroot() -> String {
-    // Taken from https://github.com/Manishearth/rust-clippy/pull/911.
-    let home = option_env!("RUSTUP_HOME").or(option_env!("MULTIRUST_HOME"));
-    let toolchain = option_env!("RUSTUP_TOOLCHAIN").or(option_env!("MULTIRUST_TOOLCHAIN"));
+static BIN_NAME: &str = "analysis_driver";
+static BIN_DIR: &str = "testbins";
 
-    match (home, toolchain) {
-        (Some(home), Some(toolchain)) => format!("{}/toolchains/{}", home, toolchain),
-        _ => option_env!("RUST_SYSROOT")
-            .or_else(|| {
-                option_env!("RUST_SRC_PATH")
-                    .map(|s| s.strip_suffix("lib/rustlib/rustc-src/rust/compiler/rustc_driver/Cargo.toml"))
-                    .flatten()
-            })
-            .expect(
-                "Need to specify RUST_SYSROOT or RUST_SRC_PATH environment variables, or use rustup or multirust",
-            )
-            .to_owned(),
-    }
+fn build_analysis() {
+    let path = format!("{BIN_DIR}/{BIN_NAME}/Cargo.toml");
+    let mut cargo = process::Command::new("cargo");
+    cargo.args(["build", "-q", "--manifest-path", &path, "--bin", BIN_NAME]);
+
+    assert!(cargo.status().expect("Failed to build analysis binary").success());
 }
 
-fn run_tests(mode: &str, path: &str, custom_args: Vec<String>) {
-    let mut config = compiletest::Config::default();
-
-    build_test_binary_once!(analysis_driver, "testbins");
-
-    let mut flags = Vec::new();
-    flags.push("--edition 2024".to_owned());
-    flags.push(format!("--sysroot {}", find_sysroot()));
-    flags.extend(custom_args);
-    config.target_rustcflags = Some(flags.join(" "));
-    config.mode = mode.parse().expect("Invalid mode");
-    config.rustc_path = path_to_analysis_driver().into();
-    config.src_base = PathBuf::from(path);
-    assert!(config.src_base.is_dir());
-
-    // Filter the tests to run
-    if let Some(filter) = env::args().nth(1) {
-        config.filters.push(filter);
-    }
-
-    if let Some(lib_path) = option_env!("RUSTC_LIB_PATH") {
-        config.run_lib_path = PathBuf::from(lib_path);
-        config.compile_lib_path = PathBuf::from(lib_path);
-    }
-
-    compiletest::run_tests(&config);
+fn run_tests(path: &str, arg: &str) {
+    compiletest::run_tests(&compiletest::Config {
+        mode: "ui".parse().expect("Invalid mode"),
+        src_base: PathBuf::from(path),
+        target_rustcflags: Some(["--edition 2024", arg].join(" ")),
+        rustc_path: format!("{BIN_DIR}/{BIN_NAME}/target/debug/{BIN_NAME}").into(),
+        ..Default::default()
+    });
 }
 
-fn test_runner(_tests: &[&()]) {
+fn test_runner(_: &[&()]) {
     // Safety: Test is single-threaded
     unsafe {
         env::set_var("RUST_BACKTRACE", "1");
     }
 
-    run_tests("ui", "tests/test_cases/definitely_initialized", vec![
-        "--analysis=DefinitelyInitializedAnalysis".into(),
-    ]);
+    build_analysis();
+    run_tests("tests/test_cases/definitely_initialized", "--analysis=DefinitelyInitializedAnalysis");
 }
